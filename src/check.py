38f84f0912d64db55c63507f89fae3a677ec7a96
Dmytro Doroshenko <dimdoroshenko@gmail.com>
2020-04-07 01:00:16 +0300

    small fixes

--- src/check.py
+++ src/check.py
@@ -10,4 +10,8 @@ for next_lvl_atr in dir(string_tools._string_tools_cython):
     print(next_lvl_atr)
 print()
 
-print(string_tools.py_is_palindrome("abba"))
+str_to_check = "abba"
+print(f"Does palindrome detected in '{str_to_check}' - {string_tools.py_is_palindrome(str_to_check)}")
+
+str_to_check = "abbab"
+print(f"Does palindrome detected in '{str_to_check}' - {string_tools.py_is_palindrome(str_to_check)}")
--- src/string_tools/cpp/str_tools.cpp
+++ src/string_tools/cpp/str_tools.cpp
@@ -3,7 +3,7 @@
 bool str_tools::cpp_is_palindrome(std::string& s){
     bool is_palindrome = true;
     int s_size = s.length();
-    for (int i = 0; i < s_size; i++) {
+    for (int i = 0; i < s_size / 2; i++) {
         is_palindrome &= (s[i] == s[s_size - i - 1]);
     }
     return is_palindrome;
